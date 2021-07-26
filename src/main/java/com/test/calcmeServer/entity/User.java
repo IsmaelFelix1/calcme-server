9a783a62615148786a58871774c82ffb54e31954
Ismael Felix <ismael.duarte017@gmail.com>
2021-07-26 12:37:24 -0300

    User entity attributes and validation implementation

--- src/main/java/com/test/calcmeServer/entity/User.java
+++ src/main/java/com/test/calcmeServer/entity/User.java
@@ -1,4 +1,34 @@
 package com.test.calcmeServer.entity;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    private String id;
+    @NotEmpty
+    @Size(min = 4, message = "o nome de usuário deve ter pelo menos 4 caracteres")
+    private String name;
+    @NotEmpty(message = "campo esta vazio")
+    @Email
+    private String email;
+    @NotEmpty
+    @Size(max = 11, message = "o usuário deve inserir até 11 caracteres")
+    private Integer phone;
+
 }
