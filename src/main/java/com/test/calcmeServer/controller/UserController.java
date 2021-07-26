ba898684ef2109a1210fa4e7f4f43d797e454fd9
Ismael Felix <ismael.duarte017@gmail.com>
2021-07-26 13:25:30 -0300

    controller class created

--- src/main/java/com/test/calcmeServer/controller/UserController.java
+++ src/main/java/com/test/calcmeServer/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.test.calcmeServer.controller;
 
+import com.test.calcmeServer.entity.User;
+import com.test.calcmeServer.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public  ResponseEntity<User> insert(@Valid @RequestBody User obj){
+        obj = service.insert(obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
