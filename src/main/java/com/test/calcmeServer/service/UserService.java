3eea5848a7a3c7f38eea062b7d4af06fd0769274
Ismael Felix <ismael.duarte017@gmail.com>
2021-07-26 12:53:04 -0300

    created service class

--- src/main/java/com/test/calcmeServer/service/UserService.java
+++ src/main/java/com/test/calcmeServer/service/UserService.java
@@ -1,4 +1,23 @@
 package com.test.calcmeServer.service;
 
+import com.test.calcmeServer.entity.User;
+import com.test.calcmeServer.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
 }
